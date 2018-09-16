6a119f59da00644fa9dd206078186e8c3a3b3910
Luka Kostić <41348897+lukakostic@users.noreply.github.com>
2018-09-16 02:04:16 +0200

    Oops.

--- BF_2_C.py
+++ BF_2_C.py
@@ -2,34 +2,29 @@
 class BF_2_C:
     bf = ""
     c = ""
-
+	
     def Compile_C(self):
         self.c = ""
-
+		
         
         for i, ch in enumerate(self.bf): #Ignores unknown commands so you can comment
           
             if(ch=='.'):
-                self.c += """printf("%c",cells[pointer]);\n"""
+                self.c += "o;\n"
             if(ch==','):
-                self.c += """
-scanf("%c%*c", & inp);
-cells[pointer] = inp[0];
-"""
+                self.c += "s;\n"
             if(ch=='+'):
-                self.c += "cells[pointer]+=1;\n"
+                self.c += "cp++;\n"
             if(ch=='-'):
-                self.c += "cells[pointer]-=1;\n"
+                self.c += "cp--;\n"
             if(ch=='>'):
-                self.c += "pointer+=1;\n"
+                self.c += "p++;\n"
             if(ch=='<'):
-                self.c += "pointer-=1;\n"
+                self.c += "p--;\n"
             if(ch=='['):
-                self.c+="""int c{} = pointer;
-while(cells[c{}]>0){{
-""".format(str(i),str(i))
+                self.c+="while(cp){{".format(str(i))
             if(ch==']'):
-                self.c+="} \n"
+                self.c+="}\n"
                 
         self.c = self.wrap(self.c)
         
@@ -39,15 +34,20 @@ while(cells[c{}]>0){{
 
     def wrap(self,txt): # Add stuff that encapsulates bf to make it work (the cells, main function, etc.)
         pre = """
-#include <stdio.h>
-#include <stdlib.h>
-
-int cells[9999]; // all cells
-int pointer = 0; // current cell
-char inp[256]; // used for input
+#include<stdio.h>
+#include<stdlib.h>
+#define cp c[p]
+#define o printf("%c",cp)
+#define s scanf("%c%*c",&i);cp=i[0]
 
 int main () 
 {
+
+unsigned char c[9999]; // all cells
+int p = 0; // current cell
+char i[256]; // used for input
+
+int j;for(j=0;j<9999;j++)c[j]=0; //reset all chars to 0
 
 
 """
@@ -55,9 +55,7 @@ int main ()
 
 while(1){}
 
-
 return 0;
-
 }
 
 """
